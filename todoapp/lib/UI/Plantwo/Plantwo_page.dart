ebf921a1f6a0df1dde2c07ff2663b864e7541c6e
Ema Baranauskaite <emabaranauskaite@MacBook-Pro-Ema.lan>
2020-05-11 21:47:14 +0300

    classes added

--- todoapp/lib/UI/Plantwo/Plantwo_page.dart
+++ todoapp/lib/UI/Plantwo/Plantwo_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/models/classes/todoitem.dart';
 import 'package:todoapp/models/global.dart';
 import 'package:todoapp/models/widgets/plantwo_todo_widgets.dart';
 
@@ -8,18 +9,36 @@ class PlantwoPage extends StatefulWidget {
 }
 
 class _PlantwoPageState extends State<PlantwoPage> {
-  List<PlantwoTodo> todoItems = [];
+  List<Task> taskList = [];
   @override
   Widget build(BuildContext context) {
-    todoItems = getList();
+    taskList = getList();
     return Container (
       color: ourblack,
-      child: ReorderableListView (
-        padding: EdgeInsets.only(top: 300),
-        children: todoItems,
-        onReorder: _onReorder,
-      ),
+      child: _buildReaorderableListSimple(context),
     );
+  }
+  Widget _buildListTile(BuildContext context, Task item) {
+    return ListTile(
+      key: Key(item.taskId),
+      title: PlantwoTodo(
+        title: item.title,
+        ),
+    );
+  }
+
+  Widget _buildReaorderableListSimple(BuildContext context) {
+    return ReorderableListView(
+      padding: EdgeInsets.only(top: 300.0),
+      children: taskList.map((Task item) => _buildListTile(context, item)).toList(),
+      onReorder: (oldIndex, newIndex) {
+        setState(() {
+          Task item = taskList[oldIndex];
+          taskList.remove(item);
+          taskList.insert(newIndex, item);
+        });
+      },
+      );
   }
 
   void _onReorder (int oldIndex, int newIndex) {
@@ -27,16 +46,16 @@ class _PlantwoPageState extends State<PlantwoPage> {
       if (newIndex > oldIndex) {
         newIndex -= 1;
       }
-      final PlantwoTodo item = todoItems.removeAt(oldIndex);
-      todoItems.insert(newIndex, item);
+      final Task item = taskList.removeAt(oldIndex);
+      taskList.insert(newIndex, item);
     });
   }
 
-    List<Widget> getList() {
+    List<Task> getList() {
       for (int i = 0; i < 10; i++) {
-        todoItems.add(PlantwoTodo(keyValue: i.toString(), title: "Hello"));
+        taskList.add(Task("MyfirstTodo " + i.toString(), false, i.toString()));
       }
-      return todoItems;
+      return taskList;
     }
 }
 
